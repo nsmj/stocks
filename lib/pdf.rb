4d1a197ffadaf9d2f5a9e41425168cf52d577ac2
Nilson de Souza Mira Júnior <mira.sni@gmail.com>
2023-12-20 11:34:44 -0300

    Handle 2 possible PDF passwords

--- lib/pdf.rb
+++ lib/pdf.rb
@@ -3,8 +3,25 @@ module Pdf
   def self.extract_pdf_data(file_path)
     file_name = File.basename(file_path).gsub('.pdf', '.txt')
 
-    system("mutool convert -p #{ENV.fetch('PDF_PASSWORD')} -F text -o txt/#{file_name} #{file_path} 2> /dev/null")
+    system(self.build_command(ENV.fetch('PDF_PASSWORD_1'), file_name, file_path))
 
+    result = self.get_data(file_name)
+
+    if result.empty?
+      system(self.build_command(ENV.fetch('PDF_PASSWORD_2'), file_name, file_path))
+      result = self.get_data(file_name)
+    end
+
+    result
+  end
+
+  private
+
+  def self.build_command(pdf_password, file_name, file_path)
+    "mutool convert -p #{pdf_password} -F text -o txt/#{file_name} #{file_path} 2> /dev/null"
+  end
+
+  def self.get_data(file_name)
     File.readlines(File.join('txt', file_name)).map(&:strip)
   end
 end
