7e1053fe172128d44a26a0b7d5307caedce14fcd
Nilson de Souza Mira Júnior <mira.sni@gmail.com>
2024-07-21 21:44:03 -0300

    Encapsulate monthly results

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,26 +3,7 @@ require 'date'
 # Class for generic reports.
 class ReportsController < ApplicationController
   def monthly_results
-    @results = Trade.select('STRFTIME(\'%d/%m/%Y\', date) AS trade_date,
-                            asset.code as ticker,
-                            quantity,
-                            asset_price,
-                            purchase,
-                            fees,
-                            total_amount,
-                            net_profit')
-                   .joins(:financial_asset)
-                   .where(date: Date.civil(filter_params[:year].to_i, filter_params[:month].to_i,
-                                           1)..Date.civil(filter_params[:year].to_i, filter_params[:month].to_i, -1))
-                   .order(:date, :ticker).map { |i| i.attributes.except('id') }
-    # TODO: Encapsulate the "remove ID" logic
-
-    @total = @results.map { |i| i['net_profit'] }.reduce do |accumulator, current|
-      accumulator = 0 if accumulator.blank?
-      accumulator += current unless current.blank?
-      accumulator
-    end
-
+    @results, @total = MonthlyResultsQuery.call(params: filter_params)
   end
 
   private
