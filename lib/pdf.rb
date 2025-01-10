f41ebb50f4d8e13a75fe00cca51d8acf19a42fc5
Nilson de Souza Mira Júnior <mira.sni@gmail.com>
2025-01-10 14:22:08 +0000

    Ajusta aspas

--- lib/pdf.rb
+++ lib/pdf.rb
@@ -1,14 +1,14 @@
 # Manipular coisas de PDF.
 module Pdf
   def self.extrai_dados_pdf(caminho_arquivo)
-    nome_arquivo = File.basename(caminho_arquivo).gsub('.pdf', '.txt')
+    nome_arquivo = File.basename(caminho_arquivo).gsub(".pdf", ".txt")
 
-    system(montar_comando(ENV.fetch('PDF_PASSWORD_1'), nome_arquivo, caminho_arquivo))
+    system(montar_comando(ENV.fetch("PDF_PASSWORD_1"), nome_arquivo, caminho_arquivo))
 
     resultado = obter_dados(nome_arquivo)
 
     if resultado.empty?
-      system(montar_comando(ENV.fetch('PDF_PASSWORD_2'), nome_arquivo, caminho_arquivo))
+      system(montar_comando(ENV.fetch("PDF_PASSWORD_2"), nome_arquivo, caminho_arquivo))
       resultado = obter_dados(nome_arquivo)
     end
 
@@ -22,6 +22,6 @@ module Pdf
   end
 
   def self.obter_dados(nome_arquivo)
-    File.readlines(File.join('txt', nome_arquivo)).map(&:strip)
+    File.readlines(File.join("txt", nome_arquivo)).map(&:strip)
   end
 end
