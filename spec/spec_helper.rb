7788d6d522e173a0827fa68c4364952f38b701ef
Nilson de Souza Mira Júnior <mira.sni@gmail.com>
2025-01-10 03:23:10 +0000

    Configurações do Rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,6 +28,14 @@ RSpec.configure do |config|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+
+    IO.read('db/database_seeds.sql').split(';').each do |sql|
+      ActiveRecord::Base.connection.execute(sql)
+    end
+  end
+
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
