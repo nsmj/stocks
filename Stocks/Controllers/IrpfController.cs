a056fea86253f1c0b85193653d7f2e83ca414a5e
Nilson de Souza Mira Júnior <mira.sni@gmail.com>
2026-07-06 17:50:42 -0300

    Move as injeções de dependência para o controller

--- Stocks/Controllers/IrpfController.cs
+++ Stocks/Controllers/IrpfController.cs
@@ -9,32 +9,24 @@ namespace Stocks.Controllers;
 /// <summary>
 /// Controller responsável por gerenciar as operações relacionadas ao IRPF.
 /// </summary>
-public class IrpfController : Controller
+public class IrpfController(
+    [FromKeyedServices("SwingTrade")] IOperacaoListable swingTradeObj,
+    [FromKeyedServices("DayTrade")] IOperacaoListable dayTradeObj,
+    [FromKeyedServices("Fii")] IOperacaoListable fiiObj,
+    LucroVendasAbaixo20kBo lucroVendasAbaixo20kBo,
+    IrrfBo irrfBo,
+    PosicaoFimAnoBo posicaoFimAnoBo,
+    IrpfRowsBuilder irpfRowsBuilder,
+    CalculadoraPrejuizoAcumuladoBo calculadoraPrejuizoAcumuladoBo
+) : Controller
 {
     /// <summary>
     /// Exibe a página de IRPF com os dados filtrados por ano.
     /// </summary>
-    /// <param name="swingTradeObj"></param>
-    /// <param name="dayTradeObj"></param>
-    /// <param name="fiiObj"></param>
-    /// <param name="lucroVendasAbaixo20kBo"></param>
-    /// <param name="irrfBo"></param>
-    /// <param name="posicaoFimAnoBo"></param>
-    /// <param name="irpfRowsBuilder"></param>
-    /// <param name="calculadoraPrejuizoAcumuladoBo"></param>
+
     /// <param name="ano"></param>
     /// <returns></returns>
-    public async Task<IActionResult> Index(
-        [FromKeyedServices("SwingTrade")] IOperacaoListable swingTradeObj,
-        [FromKeyedServices("DayTrade")] IOperacaoListable dayTradeObj,
-        [FromKeyedServices("Fii")] IOperacaoListable fiiObj,
-        [FromServices] LucroVendasAbaixo20kBo lucroVendasAbaixo20kBo,
-        [FromServices] IrrfBo irrfBo,
-        [FromServices] PosicaoFimAnoBo posicaoFimAnoBo,
-        [FromServices] IrpfRowsBuilder irpfRowsBuilder,
-        [FromServices] CalculadoraPrejuizoAcumuladoBo calculadoraPrejuizoAcumuladoBo,
-        string ano
-    )
+    public async Task<IActionResult> Index(string ano)
     {
         IrpfViewModel irpfViewModel = new();
 
